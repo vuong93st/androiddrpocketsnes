912621b5ff5663bf052d1e509d58ba8da87e667b
moment.developer <moment.developer@2fa57e67-654a-43dc-3be6-c90ee30b644f>
2010-05-20 15:07:17 +0000

    Initial checkin

---
+++ Application.mk
@@ -0,0 +1,6 @@
+APP_PROJECT_PATH := /path/to/android-ndk-r3/apps/AndroidDrPocketSNES/project
+APP_BUILD_SCRIPT := /path/to/android-ndk-r3/apps/AndroidDrPocketSNES/project/Android.mk
+APP_MODULES      := \
+		    libemu \
+		    libsnes \
+		    libemusound
